b152d673463260896252e5ae5e1d9fa6f1447e8a
Sophie Huebler <sophhuebler@gmail.com>
2026-08-09 14:36:20 -0600

    extend qc time

--- chpc/studies/Fujimoto2024.sh
+++ chpc/studies/Fujimoto2024.sh
@@ -127,6 +127,6 @@ DENOISE_TIME="7:00:00"
 # MAP_INPUT_SEQS="$REPO_ROOT/$STUDY/Mapped/qc-seqs.qza"
 #
 # --- Resources (override chpc/submit.sh defaults) ----------------------------
-QC_TIME="07:00:00";  QC_MEM="16G";  QC_THREADS=8
-MAP_TIME="07:00:00"; MAP_MEM="24G"; MAP_THREADS=8
+QC_TIME="016:00:00";  QC_MEM="16G";  QC_THREADS=8
+MAP_TIME="01:00:00"; MAP_MEM="24G"; MAP_THREADS=8
 # =============================================================================
